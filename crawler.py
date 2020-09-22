28930e6978ca95d55f6fb225490d30daa78bd312
Sascha Alexander Hornig <sascha.hornig(at)gmail.com>
2020-09-22 20:19:23 +0200

    Get login data

--- crawler.py
+++ crawler.py
@@ -1,3 +1,8 @@
 import webbrowser
 
-webbrowser.open('https://www.instagram.com/')
+webbrowser.open('https://www.instagram.com/accounts/login/', new=2, autoraise=True)
+
+
+def get_login():
+    user_name = input("Please provide your Instagram account name: ")
+    password = input("Please provide your Instagram password: ")
